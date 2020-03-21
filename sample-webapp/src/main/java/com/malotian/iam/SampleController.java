f01393903353a8c2c0193dd2b4216af1092b0313
Harjinder Dhanjal <malotian@gmail.com>
2020-03-21 11:43:33 -0400

    final

--- sample-webapp/src/main/java/com/malotian/iam/SampleController.java
+++ sample-webapp/src/main/java/com/malotian/iam/SampleController.java
@@ -13,11 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SampleController {
 
 	@GetMapping("/")
-	public String mainWithParam(@RequestParam Map<String, String> params, @RequestHeader Map<String, String> headers, Model model) {
+	public String mainWithParam(@RequestParam final Map<String, String> params,
+			@RequestHeader final Map<String, String> headers, final Model model) {
 		model.addAttribute("params", params);
 		model.addAttribute("headers", headers);
 
-		Map<String, Object> contents = new HashMap<String, Object>();
+		final Map<String, Object> contents = new HashMap<String, Object>();
 		contents.put("params", params);
 		contents.put("headers", headers);
 		model.addAttribute("contents", contents);
